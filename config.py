fc29b34ac7acee14e924b77e057190822a8cfb07
mwangisam203 <mwangisam203@gmail.com>
2026-06-22 07:01:33 -0400

    add secure auth cookie setting

--- config.py
+++ config.py
@@ -13,6 +13,7 @@ class Settings(BaseSettings):
     secret_key: SecretStr
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
+    auth_cookie_secure: bool | None = None
 
   
     # S3 Configuration
